441f44c33497f4a960194e372e3376fc95a2d480
mega <adad03860@gmial.com>
2026-04-23 21:49:17 +0800

    feat: 实现 load_file 从 RDB 文件恢复数据

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,10 @@
 
 int main() {
     skiplist<int,std::string> sl(4);
-    sl.insert(1,"one");
-    sl.insert(2,"two");
+    sl.insert(1,"one and one");
+    sl.insert(2,"two and two");
     sl.insert(3,"three");
-    sl.insert(4,"four");
-    sl.show();
-    sl.delete_node(2);
-    sl.show();
+    sl.insert(5,"five");
     sl.dump_file();
     return 0;
 }
