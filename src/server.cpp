0695c2ebd70c1a61d0209094583f56f3c069106e
mega <adad03860@gmial.com>
2026-05-16 22:57:53 +0800

    feat: server 基础 socket accept/read/write 框架

--- src/server.cpp
+++ src/server.cpp
@@ -10,17 +10,20 @@ constexpr int BUFFER_SIZE = 1024;
 
 int main()
 {
-    int server_fd = socket(AF_INET,SOCK_STREAM,0) ;
-    if(server_fd == -1)
+    //获取socket
+    int serverfd = socket(AF_INET,SOCK_STREAM,0);
+    if(serverfd == -1)
     {
-        std::cout<<"socket failed"<<std::endl;
+        std::cerr<<"创建socket失败！"<<'\n';
         return -1;
     }
-
-    int opt = 1 ;
-    if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt) < 0))
+    //配置socket
+    int opt = 1;
+    if(setsockopt(serverfd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt))<0)
     {
-        std::cerr<<"Warning: 端口复用设置失败" <<std::endl;
+        std::cerr<<"设置socket选项失败！"<<'\n';
+        close(serverfd);
+        return -1;
     }
 
     struct sockaddr_in address;
@@ -28,21 +31,35 @@ int main()
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
-    
-    if(bind(server_fd,(struct sockaddr*)&address,sizeof(address))<0)
+    if(bind(serverfd,(struct sockaddr*)&address,sizeof(address))<0)
     {
-        std::cerr<<"error:端口绑定失败"<<PORT<<std::endl;
-        close(server_fd);
+        std::cerr<<"绑定socket失败！"<<'\n';
+        close(serverfd);
+        return -1;
+    }
+
+    if(listen(serverfd,5)<0)
+    {
+        std::cerr<<"监听socket失败！"<<'\n';
+        close(serverfd);
         return -1;
     }
 
     while(true)
     {
         struct sockaddr_in client_addr;
-        socklen_t client_addr_len = sizeof( client_addr );
+        socklen_t client_len = sizeof(client_addr);
 
-        std::cout<<"等待新的客户端连接中:........."<<std::endl;
+        std::cout<<"等待客户端连接...\n";
 
-        
+        int clientfd = accept(serverfd,(struct sockaddr*)&client_addr,&client_len);
+        if(clientfd == -1 )
+        {
+            std::cout<<"接受客户端连接失败！"<<'\n';
+            continue;
+        }
+
+        std::cout<<"客户端连接成功！\n";
     }
+
 }
