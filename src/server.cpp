62c30e0dc867b2cec7f795fedfb2f56bb7353156
mega <adad03860@gmial.com>
2026-05-17 16:21:54 +0800

    feat: server 改为 thread-per-connection 多线程模型

--- src/server.cpp
+++ src/server.cpp
@@ -14,6 +14,71 @@ constexpr int BUFFER_SIZE = 1024;
 
 skiplist<std::string, std::string> kv;
 
+void handle_client(int clientfd ,skiplist<std::string,std::string>& kv)
+{
+    std::string buffer;
+        char temp[BUFFER_SIZE];
+        while(true)
+        {
+            ssize_t bytes = read(clientfd,temp,BUFFER_SIZE - 1);
+            if(bytes < 0)
+            {
+                std::cerr<<"读取客户端数据失败！"<<'\n';
+                break;
+            }
+            else if(bytes == 0)
+            {
+                std::cout<<"客户端断开连接！\n";
+                break;
+            }
+            buffer.append(temp, bytes);
+            size_t pos;
+            while((pos = buffer.find('\n')) != std::string::npos)
+            {
+                std::string line = buffer.substr(0,pos);
+                buffer.erase(0,pos+1);
+                // std::cout<<"收到客户端消息: "<<line<<'\n';
+                if(!line.empty() && line.back() == '\r')
+                    line.pop_back();
+                std::istringstream iss(line);
+                std::string cmd,key,val;
+                iss>>cmd>>key>>val;
+                std::string reponse;
+                if(cmd == "SET")
+                {
+                    bool result =kv.insert(key,val);
+                    if(result)
+                    reponse = "OK(INSERT)\n";
+                    else reponse = "OK(UPDATE)\n";
+                }
+                else if(cmd == "GET")
+                {
+                    auto result = kv.search(key);
+                    if(result.has_value())
+                        reponse = result.value() + "\n";
+                    else reponse = "NOT_FOUND\n";
+                }
+                else if(cmd == "DEL") 
+                {
+                    bool result = kv.delete_node(key);
+                    if(result)
+                        reponse = "OK(DELETE)\n";
+                    else reponse = "NOT_FOUND\n";
+                }
+                else if(cmd == "COUNT")
+                {
+                    reponse = std::to_string(kv.get_size()) + "\n";
+                }
+                else {
+                    write(clientfd, "ERROR\n", 6);
+                    continue;
+                }
+                write(clientfd, reponse.c_str(), reponse.size());
+            }
+        }
+        close(clientfd);
+}
+
 int main()
 {
     //获取socket
@@ -66,67 +131,8 @@ int main()
         }
 
         std::cout<<"客户端连接成功！\n";
-        std::string buffer;
-        char temp[BUFFER_SIZE];
-        while(true)
-        {
-            ssize_t bytes = read(clientfd,temp,BUFFER_SIZE - 1);
-            if(bytes < 0)
-            {
-                std::cerr<<"读取客户端数据失败！"<<'\n';
-                break;
-            }
-            else if(bytes == 0)
-            {
-                std::cout<<"客户端断开连接！\n";
-                break;
-            }
-            buffer.append(temp, bytes);
-            size_t pos;
-            while((pos = buffer.find('\n')) != std::string::npos)
-            {
-                std::string line = buffer.substr(0,pos);
-                buffer.erase(0,pos+1);
-                // std::cout<<"收到客户端消息: "<<line<<'\n';
-                if(!line.empty() && line.back() == '\r')
-                    line.pop_back();
-                std::istringstream iss(line);
-                std::string cmd,key,val;
-                iss>>cmd>>key>>val;
-                std::string reponse;
-                if(cmd == "SET")
-                {
-                    bool result =kv.insert(key,val);
-                    if(result)
-                    reponse = "OK(INSERT)\n";
-                    else reponse = "OK(UPDATE)\n";
-                }
-                else if(cmd == "GET")
-                {
-                    auto result = kv.search(key);
-                    if(result.has_value())
-                        reponse = result.value() + "\n";
-                    else reponse = "NOT_FOUND\n";
-                }
-                else if(cmd == "DEL") 
-                {
-                    bool result = kv.delete_node(key);
-                    if(result)
-                        reponse = "OK(DELETE)\n";
-                    else reponse = "NOT_FOUND\n";
-                }
-                else if(cmd == "COUNT")
-                {
-                    response = std::to_string(kv.get_size()) + "\n";
-                }
-                else {
-                    write(clientfd, "ERROR\n", 6);
-                    continue;
-                }
-                write(clientfd, reponse.c_str(), reponse.size());
-            }
-        }
-        close(clientfd);
+        std::thread t(handle_client,clientfd,std::ref(kv));
+        t.detach();
     }
 
 }
